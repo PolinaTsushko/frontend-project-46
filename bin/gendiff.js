4d99c8af96b91cc21acd1934a1acc2bcc38367b7
Polina Ts <linatra09@gmail.com>
2024-02-11 17:08:03 +0300

    second step: add arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'output usage information')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
