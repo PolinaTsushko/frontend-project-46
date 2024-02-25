a3437ccc93be59dc01866747bcce917c18ff955d
Polina Ts <linatra09@gmail.com>
2024-02-25 16:28:57 +0300

    step 4, sorted genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import parseJsone from '../src/index.js';
-
+// import parseJsone from '../src/index.js';
+import genDiff from '../src/index.js';
 const program = new Command();
 
 program
@@ -12,9 +12,11 @@ program
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const argument1 = parseJsone(filepath1);
-    const argument2 = parseJsone(filepath2);
-    console.log(argument1, argument2);
+    // const argument1 = parseJsone(filepath1);
+    // const argument2 = parseJsone(filepath2);
+  
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import _ from 'lodash';
 
 const parseJsone = (argument) => {
   const result = JSON.parse(fs.readFileSync(path.resolve(argument), 'utf8'));
@@ -7,4 +8,36 @@ const parseJsone = (argument) => {
   return result;
 };
 
-export default parseJsone;
+const genDiff = (filepath1, filepath2) => {
+
+  const object1 = parseJsone(filepath1);
+  const object2 = parseJsone(filepath2);
+
+  const firstArgumentKeys = Object.keys(object1);
+  const secondArgumentKeys = Object.keys(object2);
+  const unionKeys = _.union(firstArgumentKeys, secondArgumentKeys);
+
+  const added = unionKeys
+    .filter((key) => !Object.hasOwn(object1, key))
+    .map((key) => [key, { sign: '+', value: object2[key] }]);
+
+  const deleted = unionKeys
+    .filter((key) => !Object.hasOwn(object2, key))
+    .map((key) => [key, { sign: '-', value: object1[key] }]);
+
+  const unchanged = unionKeys
+    .filter((key) => Object.hasOwn(object2, key) && object1[key] === object2[key])
+    .map((key) => [key, { sign: ' ', value: object1[key] }]);
+
+  const changed = firstArgumentKeys
+    .filter((key) => Object.hasOwn(object2, key) && object1[key] !== object2[key])
+    .map((key) => [key, { sign1: '-', value1: object1[key], sign2: '+', value2: object2[key]}]);
+
+  const entries = [...added, ...deleted, ...changed, ...unchanged];
+  const sortedByEntries = _.sortBy((entries));
+  const result = Object.fromEntries(sortedByEntries);
+
+  return result;
+};
+
+export default genDiff;
