bf3ce439b2e6cb325bffa6d112e130084dd94b4a
Polina Ts <linatra09@gmail.com>
2024-03-07 14:30:00 +0300

    add txt files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,15 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getPathFromFixtures = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getPathFromFixtures = (filePath) => path.join(__dirname, '..', '__fixtures__', filePath);
+const readFixture = (filePath) => readFileSync(getPathFromFixtures(filePath), 'utf8');
+
+const flatJson = readFixture('flatjson.txt');
+const flatYml = readFixture('flatyml.txt');
 
 const filepathJson1 = getPathFromFixtures('file1.json');
 const filepathJson2 = getPathFromFixtures('file2.json');
@@ -12,16 +17,7 @@ const filepathJson2 = getPathFromFixtures('file2.json');
 const filepathYml1 = getPathFromFixtures('file1.yml');
 const filepathYml2 = getPathFromFixtures('file2.yml');
 
-const flat = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-
 test('genDiff', () => {
-  expect(genDiff(filepathJson1, filepathJson2)).toEqual(flat);
-  expect(genDiff(filepathYml1, filepathYml2)).toEqual(flat);
+  expect(genDiff(filepathJson1, filepathJson2)).toEqual(flatJson);
+  expect(genDiff(filepathYml1, filepathYml2)).toEqual(flatYml);
 });
