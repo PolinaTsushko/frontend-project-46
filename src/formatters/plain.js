796ffd65f014b141a77322aed55b687fbbdc2f3e
Polina Ts <linatra09@gmail.com>
2024-03-27 19:31:32 +0300

    fix plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,37 +4,31 @@ const stringify1 = (value) => {
   const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) {
       return '[complex value]';
-    }
-
-    if (typeof value === 'string') {
-      return `${currentValue}`
-
     } 
 
-  const lines = Object
-    .entries(currentValue)
-    .map(([key, val]) => {
-      switch (val.sign) {
-        case 'added':
-          return `Property ${key} was added with value: ${stringify1(val.value)}`;
-        case 'deleted':
-          return `Property ${key} was removed`;
-        case 'changed':
-          return `Property ${key} was updated. From ${stringify1(val.value1)} to ${stringify1(val.value2)}`;
-        case 'hasChildren':
-          return `${iter(val.value, key)}`;
-        default:
-          return [];
-      }
-    });
-  return [
-    '{',
-    ...lines,
-  ].join('\n');
-};
-return iter(value, 1);
-};
+    if (typeof currentValue === 'string') {
+      return `${currentValue}`;
+    }
 
-
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => {
+        switch (val.sign) {
+          case 'added':
+            return `Property ${key} was added with value: ${iter(val.value)}`;
+          case 'deleted':
+            return `Property ${key} was removed`;
+          case 'changed':
+            return `Property ${key} was updated. From ${iter(val.value1)} to ${iter(val.value2)}`;
+          case 'hasChildren':
+            return `${iter(val.value, key)}`;
+          default:
+            return [];
+        }
+      }).join('\n');
+    return lines;
+  };
+  return iter(value, '');
+};
 
 export default stringify1;
