537c0af667141d909c61de2fe43c8c45c4b019a4
Polina Ts <linatra09@gmail.com>
2024-04-05 00:10:19 +0300

    fixindex.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,11 @@
-import stylish from "./stylish.js";
+import stringify from "./stylish.js";
 import plain from "./plain.js";
 import json from "../json.js";
 
 const formatter = (value, formatName) => {
     switch (formatName) {
         case 'stylish':
-            return stylish(value);
+            return stringify(value);
         case 'plain':
             return plain(value);
         case 'json':
